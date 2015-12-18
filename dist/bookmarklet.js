7a3cc455f2d8bb74e0475523b55fc13dedabf26e
Adi Mora <adi.mora@gamesys.co.uk>
2015-12-18 15:08:53 +0000

    release

--- dist/bookmarklet.js
+++ dist/bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){var script=document.createElement('script');script.src='//rawgit.com/adireddy/perf-plus/2e40b794209b2bb23b680ecc9f48e0c377e5dd3e/dist/perf-plus.min.js';document.head.appendChild(script);script.onload=function(){window.perf = new PerfPlus();window.perf.start();}}())
+javascript:(function(){var script=document.createElement('script');script.src='//cdn.rawgit.com/adireddy/perf-plus/c4fb055c9139843aa527ec01a21f15da0c183ffb/dist/perf-plus.min.js';document.head.appendChild(script);script.onload=function(){window.perf = new PerfPlus();window.perf.start();}}())
